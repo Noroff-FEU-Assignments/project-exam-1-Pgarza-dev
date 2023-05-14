c98650472a2c0fd77a9db7be8c2e5bf1cc2f67fd
Pablo Garza <p.garza1983@gmail.com>
2023-05-14 07:28:45 -0700

    uncommented utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,15 @@
-// const baseUrl = 'https://pgarza-dev.com/wp-json/wp/v2/posts' 
-// const endPoint = ''
-// const specificEndPoint = ''
-// async function fetchData(baseUrl, endPoint, specificEndPoint) {
-// try {   
-//     const response = await fetch(url)
-//     const data = await response.json()
-//     return data
-// }catch(error){
-//     console.log({error: 
-//         'Something went wrong with the API request'})
-// }
-// }
+const baseUrl = 'https://pgarza-dev.com/wp-json/wp/v2/posts' 
+const endPoint = ''
+const specificEndPoint = ''
+async function fetchData(baseUrl, endPoint, specificEndPoint) {
+try {   
+    const response = await fetch(baseUrl)
+    const data = await response.json()
+    return data
+}catch(error){
+    console.log({error: 
+        'Something went wrong with the API request'})
+}
+}
 
-// export {fetchData, baseUrl, endPoint, specificEndPoint}
+export {fetchData, baseUrl, endPoint, specificEndPoint}
