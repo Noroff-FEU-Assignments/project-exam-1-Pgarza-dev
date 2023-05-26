cbe9afdd5af08f4d5eb88e1bb773a5533e86e05f
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-05-27 00:11:58 +0100

    add latestfightpost.js

---
+++ js/latestfightposts.js
@@ -0,0 +1,24 @@
+const tagsUrl = 'https://pgarza-dev.com/wp-json/wp/v2/tags';
+const baseUrl = 'https://pgarza-dev.com/wp-json/wp/v2/posts'
+// const totalPosts = '?per_page=100'
+// const endPoint = '?category_name=fight-posts'
+
+async function getPost() {
+    try {
+        const response = await fetch(baseUrl);
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log({
+        error:
+            'Something went wrong with the API request'
+        })
+
+    }finally{
+        console.log('API request completed.')
+    }
+}
+
+fetch(baseUrl)
+
